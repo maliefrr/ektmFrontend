8d3d931ed4fed522ea3c66cb5c978e623cffa6b4
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2023-06-22 17:56:07 +0800

    change form in show profile pages

--- src/pages/ShowProfile.jsx
+++ src/pages/ShowProfile.jsx
@@ -9,7 +9,6 @@ const ShowProfile = (props) => {
         name : "",
         prodi : "",
         nim : "",
-        gol_darah : "",
         jenis_kelamin : "",
         pas_foto: "",
         alamat: "",
@@ -19,12 +18,11 @@ const ShowProfile = (props) => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get(`https://ektm-backend.up.railway.app/api/mahasiswa/profile/${username}`)
+                const response = await axios.get(`https://ektm-backend.up.railway.app//api/mahasiswa/profile/${username}`)
                 setData({
                     name : response.data.mahasiswa.name,
                     prodi: response.data.mahasiswa.prodi,
                     nim: response.data.mahasiswa.nim,
-                    gol_darah: response.data.mahasiswa.gol_darah,
                     jenis_kelamin: response.data.mahasiswa.jenis_kelamin,
                     pas_foto: response.data.mahasiswa.pas_foto,
                     alamat: response.data.mahasiswa.alamat,
@@ -37,7 +35,7 @@ const ShowProfile = (props) => {
         }
         fetchData()
     },[username])
-    const {name,prodi,nim,gol_darah,jenis_kelamin,pas_foto,alamat, status, angkatan} = data
+    const {name,prodi,nim,jenis_kelamin,pas_foto,alamat, status, angkatan} = data
     return (
         <div className='my-8'>
             <img src={`${pas_foto}`} alt={`Profile ${name}`} className="w-52 h-52 rounded-md object-cover mx-auto mb-5"/>
@@ -48,20 +46,7 @@ const ShowProfile = (props) => {
                     <InputForm id='prodi' type='text' name='prodi' placeholder="Program Studi" label="Program Studi" value={prodi} disable={false} class="mb-2"/>
                     <InputForm id='angkatan' type='text' name='angkatan' placeholder="Angkatan" label="Angkatan" value={angkatan} disable={false} class="mb-2"/>
                     <InputForm id='alamat' type='text' name='alamat' placeholder="Alamat" label="Alamat" value={alamat} disable={false} class="mb-2"/>
-                    <label htmlFor='gol_darah' className="sm:mb-8">
-                        <div className="text-slate-800 mb-2">
-                            Golongan Darah
-                        </div>
-                    <select className="border rounded shadow-lg py-1 px-3 focus:ring-1 focus:border-sky-600 focus:ring-sky-500 focus:outline-none w-full invalid:focus:border-red-500 invalid:focus:ring-red-500 peer" value={gol_darah} name="gol_darah" disabled={false}>
-                        <option value="default" disabled>Golongan Darah</option>
-                        <option value="A">A</option>
-                        <option value="B">B</option>
-                        <option value="AB">AB</option>
-                        <option value="O">O</option>
-                        <option value="-">-</option>
-                    </select>
-                    </label> 
-                    <label htmlFor='gol_darah' className="sm:mb-8">
+                    <label htmlFor='jenis_kelamin' className="sm:mb-8">
                         <div className="text-slate-800 mb-2">
                             Jenis Kelamin
                         </div>
